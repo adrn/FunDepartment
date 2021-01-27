5d6093e540e9f9b6da80952fc11109e6ae349c9f
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-01-26 22:57:57 -0500

    velocity tools

---
+++ subframe/velocity.py
@@ -0,0 +1,135 @@
+# Standard library
+
+# Third-party
+from astropy.constants import c as speed_of_light
+from astropy.stats import sigma_clip
+import astropy.units as u
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+from specutils import Spectrum1D
+
+# This project
+# ...
+
+AA = u.angstrom
+
+
+def doppler_factor(dv):
+    return np.sqrt((speed_of_light + dv) / (speed_of_light - dv))
+
+
+def shift_and_interpolate(ref_spectrum, dv, target_wavelength):
+    """
+    positive dv = shifting reference spectrum to red
+
+    Parameters
+    ----------
+
+    """
+    shifted_wvln = doppler_factor(dv) * ref_spectrum.wavelength.to_value(AA)
+    idx = shifted_wvln.argsort()
+    flux_interp = InterpolatedUnivariateSpline(
+        shifted_wvln[idx],
+        ref_spectrum.flux.value[idx],
+        k=3, ext=3)
+    return flux_interp(target_wavelength.to_value(AA))
+
+
+def normalize_ref_to_frame(frame_spectrum, ref_spectrum, deg=4,
+                           sigma_clip_flux=True):
+    ref_flux_on_frame_grid = shift_and_interpolate(
+        ref_spectrum, 0.*u.km/u.s, frame_spectrum.wavelength)
+
+    wvln = frame_spectrum.wavelength.to_value(u.angstrom)
+    ref_wvln = np.median(wvln)
+
+    # Design matrix
+    d_wvln = wvln - ref_wvln
+    terms = [ref_flux_on_frame_grid * d_wvln**i
+             for i in range(deg+1)]
+    M = np.stack(terms).T
+
+    if sigma_clip_flux:
+        clipped = sigma_clip(frame_spectrum.flux)
+        n_mask = clipped.mask.sum()
+        for n in range(1024):  # MAX ITER / infinity
+            Cinv = np.ones(len(wvln))
+            Cinv[clipped.mask] = 0.
+
+            coeffs = np.linalg.solve(((M.T * Cinv) @ M),
+                                     (M.T * Cinv) @ frame_spectrum.flux)
+            warped_flux = M @ coeffs
+
+            clipped = sigma_clip(warped_flux - frame_spectrum.flux, sigma=5)
+            if (clipped.mask.sum() - n_mask) < 4 and n > 2:
+                break
+
+            n_mask = clipped.mask.sum()
+
+        clip_mask = clipped.mask
+
+    else:
+        coeffs = np.linalg.solve(M.T @ M,
+                                 M.T @ frame_spectrum.flux)
+        clip_mask = None
+
+    # Design matrix for (larger) apVisit spectrum
+    d_wvln = ref_spectrum.wavelength.to_value(u.angstrom) - ref_wvln
+    terms = [ref_spectrum.flux.value * d_wvln**i
+             for i in range(deg+1)]
+    M = np.stack(terms).T
+    normed_spectrum = Spectrum1D(
+        M @ coeffs,
+        spectral_axis=ref_spectrum.wavelength)
+
+    return normed_spectrum, clip_mask
+
+
+def cross_correlate(frame_spectrum, normed_ref_spectrum,
+                    K_half=2, dv=4.*u.km/u.s, clip_mask=None):
+
+    if clip_mask is None:
+        clip_mask = np.zeros(len(frame_spectrum.flux), dtype=bool)
+
+    terms = []
+    vs = np.arange(-K_half, K_half+1) * dv
+    for v in vs:
+        shifted_flux = shift_and_interpolate(normed_ref_spectrum,
+                                             v,
+                                             frame_spectrum.wavelength)
+        terms.append(shifted_flux)
+    M = np.stack(terms).T
+
+    Cinv = 1 / frame_spectrum.uncertainty.array**2
+    Cinv[clip_mask] = 0
+
+    denom = np.sqrt(np.diag((M.T * Cinv) @ M) * ((frame_spectrum.flux.T * Cinv) @ frame_spectrum.flux))
+    crosscorr = ((M.T * Cinv) @ frame_spectrum.flux) / denom
+
+    return crosscorr, vs
+
+
+def estimate_kernel(frame_spectrum, normed_ref_spectrum,
+                    K_half=2, dv=4.*u.km/u.s, clip_mask=None):
+
+    if clip_mask is None:
+        clip_mask = np.zeros(len(frame_spectrum.flux), dtype=bool)
+
+    terms = []
+    vs = np.arange(-K_half, K_half+1) * dv
+    for v in vs:
+        shifted_flux = shift_and_interpolate(normed_ref_spectrum,
+                                             v,
+                                             frame_spectrum.wavelength)
+        terms.append(shifted_flux)
+    M = np.stack(terms).T
+
+    # Cinv = np.ones(len(frame_spectrum.flux))
+    Cinv = 1 / frame_spectrum.uncertainty.array**2
+    Cinv[clip_mask] = 0
+
+    kernel = np.linalg.solve((M.T * Cinv) @ M,
+                             (M.T * Cinv) @ frame_spectrum.flux)
+    kernel_cov = np.linalg.inv((M.T * Cinv) @ M)
+
+    return kernel, kernel_cov, vs
