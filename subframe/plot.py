3b6e647db3bead3c5811c618a4114e04f587cb7f
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-01-27 10:04:36 -0500

    also return filenames

--- subframe/plot.py
+++ subframe/plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # This project
+from .config import plot_path
 from .utils import AA
 
 FRAME_COLOR = 'tab:purple'
@@ -15,18 +16,25 @@ SPEC_STYLE = dict(marker='', ls='-', lw=1, drawstyle='steps-mid')
 def plot_visit_frames(visit):
     spectra = visit.load_frame_spectra()
 
-    fig, ax = plt.subplots(1, 1, figsize=(12, 10))
+    fig, ax = plt.subplots(1, 1, figsize=(12, 10),
+                           constrained_layout=True)
 
     ax.plot(visit.spectrum.wavelength,
             visit.spectrum.flux / np.nanmedian(visit.spectrum.flux),
             color=VISIT_COLOR, **SPEC_STYLE)
 
-    for i, s in enumerate(spectra.values()):
-        ax.plot(s.wavelength,
+    for i, (frame, s) in enumerate(spectra.items()):
+        ax.plot(s.wavelength.value,
                 s.flux / np.nanmedian(s.flux) + i + 1,
                 color=FRAME_COLOR, **SPEC_STYLE)
+        ax.text(s.wavelength.value.min(), 2+i+0.1, str(frame))
 
-    return fig
+    ax.yaxis.set_visible(False)
+    ax.set_xlabel(f'wavelenth [{AA:latex_inline}]')
+    ax.set_title(f"{visit['VISIT_ID'].strip()}")
+
+    filename = plot_path / f"{visit['APOGEE_ID']}/{visit['VISIT_ID']}-raw.png"
+    return fig, filename
 
 
 def plot_normalized_ref_spectrum(visit, frame_name,
@@ -51,7 +59,7 @@ def plot_normalized_ref_spectrum(visit, frame_name,
             color=VISIT_COLOR, label='raw visit spectrum',
             **SPEC_STYLE)
     ax.legend(loc='lower left')
-    ax.set_title(f"{visit['APOGEE_ID']}, frame={frame_name}")
+    ax.set_title(f"{visit['VISIT_ID'].strip()}, frame={frame_name}")
 
     ax = axes[1]
     ax.plot(normed_ref_spectrum.wavelength.to_value(AA),
@@ -61,5 +69,7 @@ def plot_normalized_ref_spectrum(visit, frame_name,
     ax.legend(loc='lower left')
     ax.set_xlabel(f'wavelength [{AA:latex_inline}]')
 
-    return fig
+    filename = (plot_path /
+                f"{visit['APOGEE_ID']}/{visit['VISIT_ID']}-{frame_name}.png")
 
+    return fig, filename
